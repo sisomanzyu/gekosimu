717eea7ca96e013dbb0ab2eba88f17efe3a9567b
sp_angle <kirufin3752@gmail.com>
2017-03-08 22:09:22 +0900

    Add files via upload

---
+++ gekosimu/js/main.js
@@ -0,0 +1,23 @@
+﻿(function () {
+
+    var canvas = null;
+    var ctx = null;
+    var entity_japan_infantry = null;
+
+    document.addEventListener("DOMContentLoaded",
+        function () {
+            loadAssets();
+        });
+
+    function loadAssets() {
+        canvas = document.getElementById('mainScreen');
+        ctx = canvas.getContext('2d');
+        entity_japan_infantry = new Image();
+        entity_japan_infantry.src = 'img/japan/entity_japan_infantry_pro.png';
+
+        entity_japan_infantry.onload = function () {
+            ctx.drawImage(entity_japan_infantry, 0, 0);
+        };
+
+    }
+})();
